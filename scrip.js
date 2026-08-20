e39b0daba31d7a93fa53d6e3443253988a688bef
leonardoalves04-stack <leonardo.alves04@escola.pr.gov.br>
2026-08-20 15:14:22 +0000

    123

---
+++ scrip.js
@@ -0,0 +1,18 @@
+const botaolike = document.querySelector ("like");
+
+botaolike.addEventListener("click",botaolike);
+
+
+function () {
+    console.log ("fui clicado");
+
+
+    let texto= botaolike.querySelector("span")
+    texto.textContent++;
+
+
+
+
+
+}
+
